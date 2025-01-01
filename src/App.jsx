3563524cee3e0e581da96f3f3910d59decb58a8e
Moroundiya <moroundiya@outlook.com>
2025-01-02 00:41:40 +0100

    Form section

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,34 @@ function App() {
 					className="pointer-events-none absolute top-10 right-0"
 				/>
 
-				<img
-					src={logo}
-					alt=""
-				/>
+				<>
+					<img
+						src={logo}
+						alt=""
+					/>
+
+					<p className="pt-20 text-6xl text-white capitalize flex flex-col items-center font-bold ">
+						<span>Your Journey to Coding Conf </span>{" "}
+						<span>2025 Starts Here!</span>
+					</p>
+					<p className="text-white text-2xl mt-5">
+						Secure your spot at next year's biggest coding conference.
+					</p>
+
+					<form
+						action=""
+						className="w-2/6 my-14">
+						<div>
+							<label
+								htmlFor="avatar"
+								className="text-xl text-white">
+								Upload Avatar
+							</label>
+							<div className="w-full h-[100px] rounded-xl bg-green-600 mt-5"></div>
+							<input type="file" />
+						</div>
+					</form>
+				</>
 			</div>
 		</div>
 	);
