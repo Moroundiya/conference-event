354b61e4a04a0cf2a7803143d6843324c48ac990
Moroundiya <moroundiya@outlook.com>
2025-01-02 23:14:52 +0100

    Ticket Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import lineTop from "./assets/images/pattern-squiggly-line-top.svg";
 import patternCircle from "./assets/images/pattern-circle.svg";
 import lineBottom from "./assets/images/pattern-squiggly-line-bottom.svg";
 import logo from "./assets/images/logo-full.svg";
-import uploadIcon from "./assets/images/icon-upload.svg";
-import infoIcon from "./assets/images/icon-info.svg";
+import { useEffect, useState } from "react";
+import { Form } from "./assets/components/Form";
+import { Ticket } from "./assets/components/Ticket";
 
 function App() {
+	const [submit, setSubmit] = useState(false);
+
+	useEffect(() => {}, [submit]);
+
 	return (
 		<div className="w-full h-auto bg-desktop-bg bg-cover bg-no-repeat bg-center relative">
 			<div
@@ -47,85 +52,11 @@ function App() {
 						Secure your spot at next year&apos;s biggest coding conference.
 					</p>
 
-					<form
-						action=""
-						className="w-full md:w-3/5 xl:w-2/6 my-14 space-y-7 lg:space-y-5">
-						<div>
-							<label
-								htmlFor="avatar"
-								className="text-lg lg:text-xl text-white">
-								Upload Avatar
-							</label>
-							<div className="w-full py-8 relative rounded-xl mt-3 lg:mt-2 border-2 border-dashed cursor-pointer border-[#757199] bg-[#1b163e8e] flex flex-col justify-center items-center">
-								<input
-									type="file"
-									className="w-full h-full absolute top-0 left-0 z-10 opacity-0 cursor-pointer"
-								/>
-
-								<div className="bg-[#332E50] rounded-lg border border-[#7571999c] p-2">
-									<img
-										src={uploadIcon}
-										alt=""
-										className="lg:w-8"
-									/>
-								</div>
-								<p className="text-sm lg:text-lg text-[#757199] mt-3">
-									Drag and drop or click to upload
-								</p>
-							</div>
-							<p className="text-[#757199] text-[13px] lg:text-sm mt-2 flex items-center space-x-1">
-								<img
-									src={infoIcon}
-									alt=""
-								/>
-								<span>Upload your photo (JPG or PNG, max size: 500KB).</span>
-							</p>
-						</div>
-
-						<div>
-							<label
-								htmlFor="name"
-								className="text-lg lg:text-xl text-white">
-								Full Name
-							</label>
-							<input
-								type="text"
-								placeholder="Adewunmi Quadri"
-								className="w-full py-2.5 px-2 text-white outline-none rounded-lg mt-2 border border-double border-[#757199] bg-[#1b163e8e]"
-							/>
-						</div>
-						<div>
-							<label
-								htmlFor="email"
-								className="text-lg lg:text-xl text-white">
-								Email Address
-							</label>
-							<input
-								type="email"
-								placeholder="example@email.com"
-								className="w-full py-2.5 px-2 text-white outline-none rounded-lg mt-2 border border-double border-[#757199] bg-[#1b163e8e]"
-							/>
-						</div>
-						<div>
-							<label
-								htmlFor="github"
-								className="text-lg lg:text-xl text-white">
-								Github Username
-							</label>
-							<input
-								type="text"
-								placeholder="@yourusername"
-								className="w-full py-2.5 px-2 text-white outline-none rounded-lg my-2 border border-double border-[#757199] bg-[#1b163e8e] flex flex-col justify-center items-center"
-							/>
-						</div>
-
-						<button
-							type="email"
-							placeholder="example@email.com"
-							className="w-full py-3 px-2 text-[#04011B] capitalize outline-none rounded-lg text-lg font-bold border-double bg-[#F67464]">
-							Generate my ticket{" "}
-						</button>
-					</form>
+					{submit ? (
+						<Ticket setSubmit={setSubmit} />
+					) : (
+						<Form setSubmit={setSubmit} />
+					)}
 				</>
 			</div>
 		</div>
---
+++ src/assets/components/Ticket.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Ticket = ({ setSubmit }) => {
+	return (
+		<>
+			<div className="w-full md:w-3/5 xl:w-2/6 my-14 space-y-7 lg:space-y-5">
+				<div>Ticket</div>
+				<button
+					type="email"
+					placeholder="example@email.com"
+					onClick={() => setSubmit(false)}
+					className="w-full py-3 px-2 text-[#04011B] capitalize outline-none rounded-lg text-lg font-bold border-double bg-[#F67464]">
+					Book another ticket
+				</button>
+			</div>
+		</>
+	);
+};
