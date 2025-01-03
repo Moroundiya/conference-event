d1c04b3b6c2186859bf6cf26b85e649da2188619
Moroundiya <moroundiya@outlook.com>
2025-01-03 03:34:46 +0100

    Ticket Page

--- src/assets/components/Ticket.jsx
+++ src/assets/components/Ticket.jsx
@@ -1,5 +1,7 @@
 import ticketBox from "../images/pattern-ticket.svg";
 import logo from "../images/logo-full.svg";
+import avatar from "../images/image-avatar.jpg";
+import github from "../images/icon-github.svg";
 // eslint-disable-next-line react/prop-types
 export const Ticket = ({ setSubmit }) => {
 	return (
@@ -12,18 +14,36 @@ export const Ticket = ({ setSubmit }) => {
 						alt=""
 					/>
 
-					<div className="relative top-0 left-0 z-10 space-y-10">
+					<div className="relative top-0 left-0 z-10 space-y-7">
 						<div className="">
 							<img
 								src={logo}
 								alt=""
 								className="w-[180px] lg:auto"
 							/>
-							<p className="text-[#757199] font-bold lg:text-xl ms-10">
+							<p className="text-[#757199] font-bold text-sm lg:text-lg ms-10">
 								Jan 31, 2025 / Austin, TX
 							</p>
 						</div>
-						<div></div>
+						<div className="flex space-x-4 lg:space-x-5 items-center">
+							<img
+								src={avatar}
+								alt=""
+								className="w-11 lg:w-14 rounded-lg"
+							/>
+							<div className="">
+								<p className="text-white text-xl lg:text-2xl font-bold ">Quadri Ayodele</p>
+								<p className="flex items-center">
+									<img
+										src={github}
+										alt=""
+									/>
+									<p className="text-[#757199] font-bold text-sm lg:text-lg">
+										Devmoroundiya
+									</p>
+								</p>
+							</div>
+						</div>
 					</div>
 				</div>
 				<button
