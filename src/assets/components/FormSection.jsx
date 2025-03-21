02261468d20b8db32c511d8cd8d58589f6e8a4dc
Moroundiya <moroundiya@outlook.com>
2025-03-21 20:19:19 +0200

    optimizing

--- src/assets/components/FormSection.jsx
+++ src/assets/components/FormSection.jsx
@@ -193,7 +193,6 @@ export const FormSection = ({
 					name="email"
 					className="w-full py-2.5 px-2 text-white outline-none rounded-lg mt-2 border border-double border-[#757199] bg-[#1b163e8e] focus:bg-[#1b163e8e] focus-within:bg-[#1b163e8e]"
 				/>
-
 				<p
 					className={`${
 						validation.email ? "text-red-600" : "text-[#757199]"
